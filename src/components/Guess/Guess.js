27acbf85054508bfff99b3573d05a092d50d1e09
Dom Trott <dominic.trott@youview.com>
2023-03-30 16:41:35 +0100

    Exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-function Guess({ word }) {
+function Guess({ word, answer }) {
+  const guessStatuses = checkGuess(word, answer);
   return (
     <p className="guess">
-      {range(0, 5).map((i) => (
-        <span key={i} className="cell">
-          {word && word[i]}
-        </span>
-      ))}
+      {range(0, 5).map((i) => {
+        const status = word ? guessStatuses[i].status : "";
+        return (
+          <span key={i} className={`cell ${status}`.trim()}>
+            {word && word[i]}
+          </span>
+        );
+      })}
     </p>
   );
 }
